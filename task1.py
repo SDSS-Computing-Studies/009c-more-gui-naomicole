6aee35427014263a045e022d12c46f3fe1bf8022
naomicole <71406713+naomicole@users.noreply.github.com>
2020-11-05 11:08:34 -0800

    .

--- task1.py
+++ task1.py
@@ -7,8 +7,8 @@ from tkinter import *
 win = tk.Tk()
 win.title("tk")
 win.geometry("500x350")
-foutput = StringVar()
-foutput.set("Answer")
+answer = StringVar()
+answer.set("A = ")
 
 def clickFunction():
     h = e4.get()
@@ -16,45 +16,43 @@ def clickFunction():
     b = e2.get()
     c = e3.get()
 
-    if a == "" and b == "" and c == "":
+    if a != "" and b != "" and c != "":
        
         a = int(a)
         b = int(b)
         c = int(c)
 
         s = (a+b+c)/2
-        A = math.sqrt(s(s-a)(s-b)(s-c))
+        A = math.sqrt(s*(s-a)*(s-b)*(s-c))
 
-        f_entry.delete(0,END)
-        f_entry.insert(0,A)
+        answer.set(A)
 
-    elif h == "" and b == "" and a != "" and c != "":
+    elif h != "" and b != "" and a == "" and c == "":
         
         b = int(b)
+        h = int(h)
 
         A = b*h/2
 
-        f_entry.delete(0,END)
-        f_entry.insert(0,A)
+        answer.set(A)
 
     else:
         A = "the area cannot be calculated\nfrom the information given"
-        f_entry.delete(0,END)
-        f_entry.insert(0,A)
+        answer.set(A)
 
 triangle = PhotoImage(file="triangle.png")
 l1 = Label(win,image=triangle)
 l2 = Label(win,text="Enter as much information about the\ntriangle shown and I will calculate the area")
+l3 = Label(win,textvariable=answer)
 b1 = Button(win,text="Calculate!",command=clickFunction)
 e1 = Entry(win,text="sidea",width=5)
 e2 = Entry(win,text="base",width=5)
 e3 = Entry(win,text="sidec",width=5)
 e4 = Entry(win,text="height",width=5)
-f_entry = Entry(win,width=10,textvariable=foutput)
 
 l1.place(x=0,y=0)
 l2.place(x=0,y=275)
-f_entry.place(x=375,y=280)
+l3.place(x=325,y=280)
 b1.place(x=250,y=280)
 e1.place(x=180,y=100)
 e2.place(x=225,y=225)
